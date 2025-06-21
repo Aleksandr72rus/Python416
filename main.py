4c36545e909207ee0f69f5081c1eae8da2dbdcfa
Aleksandr72rus <613086@mail.ru>
2025-06-21 18:59:18 +0500

    hw40

--- main.py
+++ main.py
@@ -6015,25 +6015,29 @@ import re
 
 # /////////////// LECTORY 38 ////////////////////
 
+#
+# import sqlite3
+#
+# with sqlite3.connect("db_3.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     SELECT *
+#     FROM T1
+#     LIMIT 2, 5
+#     """)
+#
+#     # for res in cur:
+#     #     print(res)
+#
+#     # res = cur.fetchall()
+#     # print(res)
+#
+#     res1 = cur.fetchone()
+#     print(res1)
+#
+#     res2 = cur.fetchmany()
+#     print(res2)
 
-import sqlite3
 
-with sqlite3.connect("db_3.db") as con:
-    cur = con.cursor()
-    cur.execute("""
-    SELECT *
-    FROM T1
-    LIMIT 2, 5
-    """)
+# ///////// LECTORY 40 ////////////////
 
-    # for res in cur:
-    #     print(res)
-
-    # res = cur.fetchall()
-    # print(res)
-
-    res1 = cur.fetchone()
-    print(res1)
-
-    res2 = cur.fetchmany()
-    print(res2)
