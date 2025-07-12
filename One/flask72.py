d4e17b795eaac0db7fd074885a426e17e146b331
Aleksandr72rus <613086@mail.ru>
2025-07-12 10:14:04 +0500

    hw41

--- One/flask72.py
+++ One/flask72.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template, request, flash
+import sqlite3
+import os
+from flask import Flask, render_template, flash, request, g
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = 'fc3cc2c1df6f31308e1f0acfff869a6306905117'
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sadjkfbaskdgfalsgdksdfs'
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, "flsk.db")))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource("sq_db.sql", "r") as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
 
 
 menu = [
@@ -41,10 +63,12 @@ def contact():
 def profile(username):
     return f"Пользователь: {username}"
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("page404.html",
                            title="Страница не найдена", menu=menu)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
